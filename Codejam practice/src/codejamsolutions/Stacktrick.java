809f0909f1bcf4c81ca5656c38fb589931a8ffa1
joshuakim <joshuakim@localhost>
2022-11-13 14:43:33 -0800

    stacktrick

---
+++ Codejam practice/src/codejamsolutions/Stacktrick.java
@@ -0,0 +1,22 @@
+package codejamsolutions;
+
+import java.lang.Thread.UncaughtExceptionHandler;
+
+public class Stacktrick {
+	static boolean killMePls = false;
+	public static void main(String[] args) throws Exception {
+		Thread.UncaughtExceptionHandler h = new UncaughtExceptionHandler() {
+			public void uncaughtException(Thread t, Throwable e) {
+				killMePls = true;
+			}
+		};
+		Thread t = new Thread(null,()->A(args),"",1<<28);
+		t.setUncaughtExceptionHandler(h);
+		t.start();
+		t.join();
+		if (killMePls) {
+			throw null;
+		}
+	}
+	public static void A(String[] args) {}
+}
