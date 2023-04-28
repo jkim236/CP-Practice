938dbd4b13bdb8732d364a6bbfa5202584b72fb9
Joshua Kim <118137134+jkim236@users.noreply.github.com>
2023-04-28 16:18:05 -0700

    Update Kevin.java
    commented to show completion date, problem statement, and solution inner workings

--- Codejam practice/src/algorithmsPractice/Kevin.java
+++ Codejam practice/src/algorithmsPractice/Kevin.java
@@ -1,5 +1,20 @@
+// Solved: Nov/14/2022
+
+/**
+* Problem statement:
+* Given a set of distinct integers from 1 to n, arrange them in a way that maximizes 
+* the minimum absolute difference between consecutive elements. The goal is to output 
+* any permutation that satisfies this condition.
+*/
 package algorithmsPractice;
 import java.util.Scanner; 
+
+/** The code takes the number of test cases and the size of the set as inputs, and 
+* then it prints a permutation of the numbers that satisfies this condition.
+* To do this, the code calculates the maximum possible value of the minimum absolute 
+* difference between consecutive elements, and then prints the permutation using a loop. 
+* Finally, the output is printed on separate lines for each test case.
+*/
 public class Kevin {
 	public static void main(String[] args) {
 		try(Scanner console = new Scanner(System.in)) {
