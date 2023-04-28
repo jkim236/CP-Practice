01e10269fc97bda3e020c3ea5a0494802bed5d62
Joshua Kim <118137134+jkim236@users.noreply.github.com>
2023-04-28 16:36:56 -0700

    Update BeautifulTree.java

--- Codejam practice/src/algorithmsPractice/BeautifulTree.java
+++ Codejam practice/src/algorithmsPractice/BeautifulTree.java
@@ -1,5 +1,17 @@
+/**
+* Date completed: Nov/14/2022
+* Problem statement:
+* Masha has a complete binary tree of height n and a permutation p of length 2n. She wants to make the tree 
+* "beautiful" where the values in its leaves are ordered from left to right 
+* in increasing order. Determine the minimum number of operations required to make the tree 
+* "beautiful" where an operation involves swapping the left and right subtrees of a non-leaf vertex.
+*/
 package algorithmsPractice;
 import java.util.*;
+
+/**
+* Utilizes dfs and divide and conquer to solve problem statement
+*/
 public class BeautifulTree {
 	public static void main(String[] args) {
 		try(Scanner console = new Scanner(System.in)) {
@@ -31,41 +43,41 @@ public class BeautifulTree {
 								count ++;
 							}
 						}
-							if (one[one.length - 1] > two[0]) {
-								for (int m = 0; m < track / 2; m ++) {
-									if (one[0] < two[m]) {
-										ans = -1;
-										running = false;
-										break;
-									}
-								}
-								if (running){
-									ans ++;
-									for (int z = 0; z < track; z ++) {
-										if(z < track / 2) {
-											combined[z] = two[z]; 
-										}
-										else {
-											combined[z] = one[z - track / 2];
-										}
-									}
-									for (int z = j; z < j + track; z ++) {
-										arr[z] = combined[z - j];
-									}
+						if (one[one.length - 1] > two[0]) {
+							for (int m = 0; m < track / 2; m ++) {
+								if (one[0] < two[m]) {
+									ans = -1;
+									running = false;
+									break;
 								}
 							}
-							
+							if (running){
+								ans ++;
+								for (int z = 0; z < track; z ++) {
+									if(z < track / 2) {
+										combined[z] = two[z]; 
+									}
+									else {
+										combined[z] = one[z - track / 2];
+									}
+								}
+								for (int z = j; z < j + track; z ++) {
+									arr[z] = combined[z - j];
+								}
+							}
 						}
-						track *= 2;
-						if (track > size) {
-							running = false;
-						}
-					}
-					System.out.println(ans);
-				}
-			}	
-		}
-	}
 
-// 3 4 7 8 1 2 5 6
+					}
+					track *= 2;
+					if (track > size) {
+						running = false;
+					}
+				}
+				System.out.println(ans);
+			}
+		}	
+	}
+}
+
+
 
