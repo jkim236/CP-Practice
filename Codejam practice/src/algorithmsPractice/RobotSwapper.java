c06e4d529a2a6d3247fb6911cea68e88454c45c4
Joshua Kim <118137134+jkim236@users.noreply.github.com>
2023-04-28 16:40:59 -0700

    Update RobotSwapper.java

--- Codejam practice/src/algorithmsPractice/RobotSwapper.java
+++ Codejam practice/src/algorithmsPractice/RobotSwapper.java
@@ -1,5 +1,12 @@
+/**
+* Date completed: Nov/17/2022
+* Problem statement: 
+* You are given a string s and a robot that currently holds an empty string t. Apply one of the following operations until s and t are both empty:
+* Remove the first character of a string s and give it to the robot. The robot will append this character to the string t.
+* Remove the last character of a string t and give it to the robot. The robot will write this character on paper.
+* Return the lexicographically smallest string that can be written on the paper.
+*/
 package algorithmsPractice;
-
 import java.util.*;
 
 
