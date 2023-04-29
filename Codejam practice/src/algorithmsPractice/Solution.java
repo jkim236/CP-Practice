073fb305b87ed02ce17945f3d66124ba37e8f467
Joshua Kim <118137134+jkim236@users.noreply.github.com>
2023-04-28 18:08:10 -0700

    Update Solution.java

--- Codejam practice/src/algorithmsPractice/Solution.java
+++ Codejam practice/src/algorithmsPractice/Solution.java
@@ -1,7 +1,33 @@
+/**
+/**
+
+* Wile builds machines that run on chain reactions.
+* Each machine consists of N modules indexed 1,2,…,N.
+* Each module may point at one other module with a lower index. If not, it points at the abyss.
+* Initiators are modules that are not pointed at by any others.
+* When a module is triggered, it triggers the module it is pointing at (if any)
+* which may trigger a third module (if it points at one), and so on.
+* Fun factor Fi of each module is known. The overall fun is the sum of the fun Wile gets from each chain reaction.
+* Wile triggers each initiator module once, in some order. The overall fun Wile gets from the session is the sum of the fun he gets from each chain reaction.
+* Input
+* The first line contains the number of test cases, T.
+* T test cases follow, each described using 3 lines.
+* Each test case starts with a line with a single integer N, the number of modules Wile has.
+* The second line contains N integers F1,F2,…,FN where Fi is the fun factor of the i-th module.
+* The third line contains N integers P1,P2,…PN. If Pi=0, that means module i points at the abyss.
+* Otherwise, module i points at module Pi.
+* Output
+* For each test case, output one line containing Case #x: y,
+* where x is the test case number (starting from 1) and y is the maximum fun Wile can have
+* by manually triggering the initiators in the best possible order.
+*/
 package algorithmsPractice;
 
 import java.util.*;
 import java.io.*;
+
+// This class contains a solution for the problem statement above. Initializes a graph with weighted nodes and utilizes a dfs and a bottom up approach to 
+// solve the problem statement. Also, avoids stack overflow error by increasing recursive stack limit
 public class Solution {
 	static boolean killMePls = false;
 	public static void main(String[] args) throws Exception {
